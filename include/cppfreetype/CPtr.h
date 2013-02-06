480c1f29e5c23a5fa4f97d45d0bc060bf23f8c4f
Josh Bialkowski <jbialk@mit.edu>
2013-02-06 10:32:05 -0500

    replaced 'openbook' in copyright with correct library

--- include/cppfreetype/CPtr.h
+++ include/cppfreetype/CPtr.h
@@ -1,7 +1,7 @@
 /*
  *  Copyright (C) 2012 Josh Bialkowski (jbialk@mit.edu)
  *
- *  This file is part of openbook.
+ *  This file is part of cppfreetype.
  *
  *  cppfreetype is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
